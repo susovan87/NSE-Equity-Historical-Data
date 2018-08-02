ffba6b1b3f458964e957de4372d4b443c966dbb2
Travis CI <travis@travis-ci.org>
2018-08-02 21:22:01 +0530

    Travis build:

--- push.sh
+++ push.sh
@@ -11,7 +11,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/susovan87/NSE-Equity-Historical-Data.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/susovan87/NSE-Equity-Historical-Data.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master 
 }
 
